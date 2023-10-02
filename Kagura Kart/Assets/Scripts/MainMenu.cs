6a5388a23c969d3adb3491e76e7a7ec1bd8af8cc
RipKowai <georgi.damyanov@edu.futuregames.nu>
2023-10-02 14:53:32 +0200

    In game Menu working

--- Kagura Kart/Assets/Scripts/MainMenu.cs
+++ Kagura Kart/Assets/Scripts/MainMenu.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public bool level1 = false;
+    public bool level2 = false;
+    public bool level3 = false;
     public void Level1()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        level1 = true;
     }
 
     public void Level2()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        level2 = true;
     }
     public void Level3()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        level3 = true;
+    }
+
+    private void BackToMainMenu()
+    {
+        if (level1 == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            level1 = false;
+        }
+        if (level2 == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+            level2 = false;
+        }
+        if (level3 == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+            level3 = false;
+        }
     }
 
     public void QuitGame()
